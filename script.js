94358ecfb1c72c4ab70509e75a85e1f12521aa6a
Dmitry Stukateev <Dmitry.Stukateev@yandex.ru>
2022-12-03 12:24:30 +0300

    Первая сдача на проверку

---
+++ script.js
@@ -0,0 +1,53 @@
+let buttonUserInfo = document.querySelector(".user-info__edit");
+let popupUserInfo = document.querySelector(".popup_edit-profile");
+let closeTabUserInfo = document.querySelector(".popup_edit-profile__close");
+function openChangeUserInfo(){
+    popupUserInfo.classList.remove("popup_edit-profile_state_disabled")
+}
+
+
+
+
+
+let formElement = document.querySelector(".popup_edit-profile__form")// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let namePage = document.querySelector(".user-info__name")
+let jobPage = document.querySelector(".user-info__job")
+
+
+let nameInput = formElement.querySelector(".popup__input_type_name")// Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector(".popup__input_type_info")// Воспользуйтесь инструментом .querySelector()
+
+function refreshValueInput(){
+    nameInput.value = namePage.textContent
+    jobInput.value = jobPage.textContent
+}
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+    function handleFormSubmit (evt) {
+        evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+        // Так мы можем определить свою логику отправки.
+        // О том, как это делать, расскажем позже.
+        namePage.textContent = nameInput.value
+        jobPage.textContent = jobInput.value
+        // Получите значение полей jobInput и nameInput из свойства value
+        closeChangeUserInfo()
+        // Выберите элементы, куда должны быть вставлены значения полей
+        refreshValueInput()
+        // Вставьте новые значения с помощью textContent
+    }
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
+
+function closeChangeUserInfo(){
+    popupUserInfo.classList.add("popup_edit-profile_state_disabled")
+    refreshValueInput()
+}
+
+buttonUserInfo.addEventListener('click', openChangeUserInfo);
+closeTabUserInfo.addEventListener('click', closeChangeUserInfo);
+
+refreshValueInput()
